fae02fa70e3934c15614c71aec919f386e8d84d1
Cheongha <sch7188@gmail.com>
2021-09-03 00:59:34 +0900

    feat: post 조회,저장,수정,삭제

--- src/main/java/org/example/springboot/domain/posts/PostsRepository.java
+++ src/main/java/org/example/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package org.example.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
+    // SpringDataJpa에서 기본 메소드만으로 해결할 수 있지만
+    // @Query가 훨씬 가독성이 좋으니 선택해서 사용 가능
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/org/example/springboot/service/posts/PostsService.java
+++ src/main/java/org/example/springboot/service/posts/PostsService.java
@@ -3,12 +3,15 @@ package org.example.springboot.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.example.springboot.domain.posts.Posts;
 import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.web.dto.PostsListResponseDto;
 import org.example.springboot.web.dto.PostsResponseDto;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
 import org.example.springboot.web.dto.PostsUpdateRequestDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -28,9 +31,27 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
     public PostsResponseDto findById (Long id){
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    // readOnly = true 트랜잭션 범위는 유지하되,
+    // 조회 기능만 남겨두어 조회 속도가 개선되기 때문에
+    // 등록, 수정, 삭제 기능이 전혀 없는 서비스 메소드에서 사용하는 것 추천
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // = .map(posts -> new PostsListResponseDto(posts))
+                .collect(Collectors.toList());
     }
 }
